4c4b986f8e91485b6ef4c9fe99454bfd7cdacace
subash kharel <subash-minu@localhost.local>
2019-12-22 10:03:26 -0600

    full implementation of swagger documentation

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # springboot-restful
+
+Swagger Endpoint: http://localhost:8080/v2/api-docs
+Swagger ui endpoint : http://localhost:8080/swagger-ui.html
--- src/main/java/com/subash/restservices/poc/restfulpoc/Student.java
+++ src/main/java/com/subash/restservices/poc/restfulpoc/Student.java
@@ -5,10 +5,13 @@ import java.util.Date;
 import javax.validation.constraints.Past;
 import javax.validation.constraints.Size;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+@ApiModel(description = "All info about the student")
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
@@ -16,8 +19,10 @@ public class Student {
 	
 	private Integer Id;
 	@Size(min =2,message ="Should be at least of size 2")
+	@ApiModelProperty(notes =" should have atleast two letters")
 	private String name;
 	@Past
+	@ApiModelProperty(notes =" birthdate should be before todays date")
 	private Date dob;
 
 }
